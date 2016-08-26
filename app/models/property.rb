7c828f889614f8bc88068132f646606643899f27
Zulchm <zulchm@student.elms.edu>
2016-08-26 12:03:24 -0400

    creates relationship between  properties read_group_properties and property

--- app/models/property.rb
+++ app/models/property.rb
@@ -2,6 +2,6 @@ class Property < ActiveRecord::Base
   validates_presence_of :name
   validates_uniqueness_of :name
 
-  has_many :read_group_properties, dependent: :destroy
+  has_many :read_group_properties, inverse_of: :property,  dependent: :destroy
 
 end
