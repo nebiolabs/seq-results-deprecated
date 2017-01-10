af36ee761770d400e2443379b26c15de46a493eb
Michael <zulchm@student.elms.edu>
2017-01-10 17:12:17 -0500

    adds email column to read_groups

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170110185353) do
+ActiveRecord::Schema.define(version: 20170110220851) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -368,6 +368,7 @@ ActiveRecord::Schema.define(version: 20170110185353) do
     t.datetime "updated_at"
     t.string   "product_number",           limit: nil
     t.string   "lot_number",               limit: nil
+    t.string   "email",                    limit: nil
   end
 
   add_index "read_groups", ["genome_id"], name: "index_read_groups_on_genome_id", using: :btree
