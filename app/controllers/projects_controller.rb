0c0d0667b0752ede63e988b426d2a9e2544c1cde
Brad Langhorst <langhorst@neb.com>
2017-05-17 07:11:13 -0400

    avoids null projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,7 +2,7 @@ class ProjectsController < ApplicationController
   respond_to :json
 
   def index
-    @projects = ReadGroup.select(:project).distinct.map{|read_group|
+    @projects = ReadGroup.select(:project).distinct.where("project is not null").map{|read_group|
       {id: read_group.project}
     }
 
